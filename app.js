6d743ea4e2d4a0fdb47d57f4729f94dec34bb88c
Lucas Smilari <lucassmilari2108@gmail.com>
2022-02-27 23:46:44 -0300

    Cambios visuales

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var pokemonsRouter = require('./routes/pokemons');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var pokemonsRouter = require("./routes/pokemons");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/pokemons', pokemonsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/pokemons", pokemonsRouter);
 
 module.exports = app;
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,18 @@
 //! GENERADOR DE NUMERO RANDOM
 const getRandomInt = (min, max) => {
-    return  Math.floor(Math.random() * (max - min)) + min; 
-  }
+	return Math.floor(Math.random() * (max - min)) + min;
+};
 
-const random = getRandomInt(1,152) //* Genera un numero del 1 al 151 y lo pasa a una constante
-  
+const random = getRandomInt(1, 152); //* Genera un numero del 1 al 151 y lo pasa a una constante
 
- 
-const fetchData = async(id) => {
-    try {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        const data = await res.json() //* Transformo la informacion a un json
-        console.log(data);
-    } catch (error) { //* En caso de haber un error salta directamente a esta linea de codigo
-        console.log(error)
-    }
-}
-fetchData(random)
+const fetchData = async (id) => {
+	try {
+		const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+		const data = await res.json(); //* Transformo la informacion a un json
+		console.log(data);
+	} catch (error) {
+		//* En caso de haber un error salta directamente a esta linea de codigo
+		console.log(error);
+	}
+};
+fetchData(random);
